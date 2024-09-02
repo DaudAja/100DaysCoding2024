6ea6796e5e248b0c63829ff631c90098034a5b75
DaudAja <cpgmng24@gmail.com>
2024-09-02 22:39:20 +0800

    hari ke 1

---
+++ hari001/Main.java
@@ -0,0 +1,16 @@
+package hari001;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+        System.out.print("Ini print");
+        System.out.println(" coding");
+            String nama = "daud";
+            int umur = 20;
+        //berguna untuk mencetak suatu codingan dengan parameter (format)
+        System.out.printf("Nama: %s, Umur: %d\n", nama, umur);
+        /*
+        komentar multiline
+        */
+    }
+}

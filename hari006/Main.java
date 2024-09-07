f528dcace0294004a55f879c2f981dfd4f2c8ca0
DaudAja <cpgmng24@gmail.com>
2024-09-07 18:27:48 +0800

    Deklarasi Variable

---
+++ hari006/Main.java
@@ -0,0 +1,18 @@
+package hari006;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //mendeklarasikan variable dengan tipe data int,string,boolean,double
+        int angka = 20;
+        String teks = "Semangat";
+        double desimal = 45.5;
+        boolean benarSalah = true;
+        
+        //mencetak variable
+        System.out.println("Hasil Variable Angka     : "+ angka);
+        System.out.println("Hasil Variable teks      : "+ teks);
+        System.out.println("Hasil Variable desimal   : "+ desimal);
+        System.out.println("Hasil Variable benarSalah: "+ benarSalah);
+    }
+}

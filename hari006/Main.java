4daef2ad1529c45abbcf8c3630d25e278ec3cfa3
DaudAja <cpgmng24@gmail.com>
2024-09-08 18:46:18 +0800

    Inisialisasi Variabel

--- hari006/Main.java
+++ hari006/Main.java
@@ -3,7 +3,8 @@ package hari006;
 public class Main {
     public static void main(String[] args) {
         
-        //mendeklarasikan variable dengan tipe data int,string,boolean,double
+        //menginisialisasikan variable dengan tipe data int,string,boolean,double
+        //tipeData namaVariable = nilai;
         int angka = 20;
         String teks = "Semangat";
         double desimal = 45.5;
---
+++ hari007/Main.java
@@ -0,0 +1,15 @@
+package hari007;
+
+public class Main {
+    public static void main(String[] args) {
+        //Mendeklarasikan Variabel
+        //tipeData namaVariabel;
+        //namaVariabel = nilai;
+        String namaDepan,namaBelakang;
+        namaDepan = "Daud";
+        namaBelakang = "Buntu Ma'dika";
+        System.out.println("Nama Depan   : "+namaDepan);
+        System.out.println("Nama Belakang: "+namaBelakang);
+        System.out.println("Nama Lengkap : "+namaDepan+" "+namaBelakang);
+    }
+}

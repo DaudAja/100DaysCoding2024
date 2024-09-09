5abf3add37490f215742cc88a9a7443e63d72433
DaudAja <cpgmng24@gmail.com>
2024-09-09 20:43:21 +0800

    Update Variabel

---
+++ hari008/Main.java
@@ -0,0 +1,21 @@
+package hari008;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String namaDepan = "Daud";
+        String namaBelakang = "Buntu Ma'dika";
+
+        System.out.println("Sebelum Diupdate");
+        System.out.println("===================");
+        System.out.println(namaDepan + " " + namaBelakang);
+
+        System.out.println("-----------------------");
+        //mengupdate isi variable sesuai keinginan
+        namaBelakang = "Bongga Lotong";
+
+        System.out.println("Sesudah Diupdate");
+        System.out.println("===================");
+        System.out.println(namaDepan + " " + namaBelakang);
+    }
+}

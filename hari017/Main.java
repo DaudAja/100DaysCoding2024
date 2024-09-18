9fa82efebe6933b31f7f0ad542e27dc8b310e272
DaudAja <cpgmng24@gmail.com>
2024-09-18 21:27:05 +0800

    Tipe data char

---
+++ hari017/Main.java
@@ -0,0 +1,19 @@
+package hari017;
+
+public class Main {
+    public static void main(String[] args) {
+        //tipe data char merupakan tipe data primitif yang hanya dapat menyimpan satu karakter saja dengan range 0 - 65.535
+        
+        //deklarasivariabel
+        char karakter_1,karakter_2,karakter_3,karakter_4;
+        
+        //inisialisasi variabel
+        karakter_1 = 'D';
+        karakter_2 = 'A';
+        karakter_3 = 'U';
+        karakter_4 = 'D';
+        
+        //Mencetak variabel
+        System.out.printf("Keluaran : %c %c %c %c %n",karakter_1,karakter_2,karakter_3,karakter_4);
+    }
+}

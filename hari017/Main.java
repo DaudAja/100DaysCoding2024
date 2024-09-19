ef734e0954ebf958c64a746b04cbd0a44f8b3bab
DaudAja <cpgmng24@gmail.com>
2024-09-19 21:48:59 +0800

    Tipe data boolean

--- hari017/Main.java
+++ hari017/Main.java
@@ -5,15 +5,17 @@ public class Main {
         //tipe data char merupakan tipe data primitif yang hanya dapat menyimpan satu karakter saja dengan range 0 - 65.535
         
         //deklarasivariabel
-        char karakter_1,karakter_2,karakter_3,karakter_4;
+        char karakter_1,karakter_2,karakter_3,karakter_4,karakter_5;
         
         //inisialisasi variabel
         karakter_1 = 'D';
         karakter_2 = 'A';
         karakter_3 = 'U';
         karakter_4 = 'D';
+        karakter_5 = '\\';
         
         //Mencetak variabel
+        System.out.println("karakter khusus : "+karakter_5);
         System.out.printf("Keluaran : %c %c %c %c %n",karakter_1,karakter_2,karakter_3,karakter_4);
     }
 }
---
+++ hari018/Main.java
@@ -0,0 +1,17 @@
+package hari018;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //tipe data boolean adalah tipe data primitif yang hanya menyimpan dua nilai yaitu true dan false
+        //deklarasi variabel
+        boolean manusia, siswa;
+
+        //inisialisai variabel
+        manusia = true;
+        siswa = false;
+
+        //mencetak variabel
+        System.out.printf("Apakah saya manusia : %b %nApakah saya   siswa : %b%n", manusia, siswa);
+    }
+}

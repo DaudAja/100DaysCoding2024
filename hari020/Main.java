89d4e00265895657d5a8c6e1f4a4e293abde057c
DaudAja <cpgmng24@gmail.com>
2024-09-21 21:26:58 +0800

    Tipe data reference Byte

---
+++ hari020/Main.java
@@ -0,0 +1,14 @@
+package hari020;
+
+public class Main {
+    public static void main(String[] args) {
+        //deklarasi variabel
+        Byte nilaiA,nilaiB;
+        //inisialisasi variabel
+        nilaiA = 90;
+        nilaiB = 80;
+        //mencetak variabel
+        System.out.println(nilaiA.compareTo(nilaiB));
+        System.out.println(nilaiB.byteValue());
+    }
+}

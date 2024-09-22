c5fe90e6e4379160f15fe684a36e104a1f5b3ca4
DaudAja <cpgmng24@gmail.com>
2024-09-22 20:20:22 +0800

    Tipe data reference Short

---
+++ hari021/Main.java
@@ -0,0 +1,13 @@
+package hari021;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Short nomor = 2024;
+        String ubah = nomor.toString();
+
+        System.out.println("Isi angka\t: " + nomor);
+        System.out.println("Berapa angka\t: " + ubah.length() + " Digit");
+    }
+}

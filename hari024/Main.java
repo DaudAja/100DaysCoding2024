d919ca7287f4db162a5ba8816f813ea49b34d61a
DaudAja <cpgmng24@gmail.com>
2024-09-25 23:45:05 +0800

    Tipe data reference Float

---
+++ hari024/Main.java
@@ -0,0 +1,12 @@
+package hari024;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Float angka1 = 8.6f;
+        Float angka2 = 4.2f;
+        
+        System.out.println(angka1.equals(angka2));
+        System.out.println(angka2.compareTo(angka1));
+    }
+}

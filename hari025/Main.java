de7f78463a578899cb96c8f38c044548da563951
DaudAja <cpgmng24@gmail.com>
2024-09-26 20:01:23 +0800

    Tipe data reference Double

---
+++ hari025/Main.java
@@ -0,0 +1,13 @@
+package hari025;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Double angka = 24.5;
+        Double angka2 = 4.5;
+
+        System.out.println("nilai terbesar = " + Math.max(angka, angka2));
+        System.out.println("nilai terkecil = " + Math.min(angka, angka2));
+    }
+}

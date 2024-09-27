285ba1c268fabd0160b89db34dcdf10056f3b229
DaudAja <cpgmng24@gmail.com>
2024-09-27 23:31:25 +0800

    Tipe data reference Character

---
+++ hari026/Main.java
@@ -0,0 +1,13 @@
+package hari026;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Character huruf1 = 'D';
+        Character huruf2 = 'A';
+       
+        System.out.println(huruf1.charValue());
+        System.out.println(huruf2.toString());
+        
+    }
+}

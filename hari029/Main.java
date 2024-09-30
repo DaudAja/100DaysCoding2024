4f1e4afc5da52af688234d6e55cf887eeabc604d
DaudAja <cpgmng24@gmail.com>
2024-09-30 23:14:52 +0800

    Tipe data Reference BigInteger

---
+++ hari029/Main.java
@@ -0,0 +1,16 @@
+package hari029;
+
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BigInteger a = new BigInteger("12000000345859035839789353");
+        BigInteger b = new BigInteger("12345678902345674860483829");
+
+        System.out.println("Penjumlahan \t= " + a.add(b)); //penjumlahan
+        System.out.println("Perkalian \t= " + b.multiply(a)); //perkalian
+
+    }
+}

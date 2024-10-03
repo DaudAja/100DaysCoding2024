dc543d658bd850d30c14c450d64b65cb51e5259b
DaudAja <cpgmng24@gmail.com>
2024-10-03 11:26:55 +0800

    Konversi Primitif ke String

---
+++ hari032/Main.java
@@ -0,0 +1,39 @@
+package hari032;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        byte a = 10;
+        String strByte = String.valueOf(a);
+        System.out.println("Hasil = "+strByte);
+        
+        short b = 15;
+        String strShort = String.valueOf(b);
+        System.out.println("Hasil = "+strShort);
+        
+        int c = 56;
+        String strInt = String.valueOf(c);
+        System.out.println("Hasil = "+strInt);
+        
+        long d = 12345;
+        String strLong = String.valueOf(d);
+        System.out.println("Hasil = "+strLong);
+        
+        float e = 24.5f;
+        String strFloat = String.valueOf(e);
+        System.out.println("Hasil = "+strFloat);
+        
+        double f = 8.5;
+        String strDouble = String.valueOf(f);
+        System.out.println("Hasil = "+strDouble);
+        
+        char g = 'D';
+        String strChar = String.valueOf(g);
+        System.out.println("Hasil = "+strChar);
+        
+        boolean h = true;
+        String strBoolean = String.valueOf(h);
+        System.out.println("Hasil = "+strBoolean);
+    }
+    
+}

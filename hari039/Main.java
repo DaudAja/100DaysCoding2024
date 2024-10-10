54c5f1de2278c9d9f8a1fb47fa6a689b1f6fc679
DaudAja <cpgmng24@gmail.com>
2024-10-10 20:00:25 +0800

    Operator Penugasan : Pengisian dan Modulo

---
+++ hari039/Main.java
@@ -0,0 +1,13 @@
+package hari039;
+
+public class Main {
+    public static void main(String[] args) {
+        int angka1 = 15;
+        int angka2 = 2;
+        
+        System.out.println("Pengisian dan modulo");
+        angka1 %= angka2;
+        
+        System.out.println("Hasil = "+angka1);
+    }
+}

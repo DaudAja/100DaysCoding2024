6a0bc4902c4390c7f11869a04ed877cde1d06130
DaudAja <cpgmng24@gmail.com>
2024-10-14 21:27:42 +0800

    Operator Perbandingan : lebih besar Sama dengan dan Lebih kecil sama dengan

---
+++ hari043/Main.java
@@ -0,0 +1,15 @@
+package hari043;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        
+        a = 76;
+        b = 24; 
+        System.out.println(a >= b);
+        
+        a = 75;
+        b = 56;
+        System.out.println(a <= b);
+    }
+}

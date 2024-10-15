9940706e574963dafd13bf7cfec3d82946db04d5
DaudAja <cpgmng24@gmail.com>
2024-10-15 22:09:29 +0800

    Operator Logika : AND (&&)

---
+++ hari044/Main.java
@@ -0,0 +1,13 @@
+package hari044;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 15;
+        
+        boolean b = a > 12 && a < 18;
+        
+        System.out.println(b);
+    }
+    
+}

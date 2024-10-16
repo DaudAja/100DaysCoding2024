415716ababecce3cae926d05383fa5f272b8e41e
DaudAja <cpgmng24@gmail.com>
2024-10-16 19:04:03 +0800

    Operator Logika : OR (||)

--- hari045/Main.java
+++ hari045/Main.java
@@ -1,11 +1,12 @@
-package hari044;
+package hari045;
 
 public class Main {
     public static void main(String[] args) {
         
-        int a = 15;
-        boolean b = a > 12 && a < 18;
+        byte a = 30;
+        
+        boolean b = a > 15 || a < 20;
+        
         System.out.println(b);
     }
-    
 }

af4522282dc65780c41125cb725dd92f1dd2368c
DaudAja <cpgmng24@gmail.com>
2024-10-17 19:55:10 +0800

    Operator Logika : Negasi/Kebalikan (!)

---
+++ hari046/Main.java
@@ -0,0 +1,13 @@
+package hari046;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        byte nilai = 15;
+        
+        boolean b = !(nilai < 14);
+        
+        System.out.println(b);
+    }
+    
+}

f99d4fe94360942f770fdbd734bd1ca517e65c73
DaudAja <cpgmng24@gmail.com>
2024-10-18 22:22:59 +0800

    Percabangan : if

---
+++ hari047/Main.java
@@ -0,0 +1,12 @@
+package hari047;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        byte nilai = 50;
+        
+        if(nilai > 30){
+            System.out.println("ya,"+nilai+" lebih besar dari 30");
+        }
+    }
+}

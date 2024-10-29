9f198fbdd955a5ff5dee15613b364505b92bbdb8
DaudAja <cpgmng24@gmail.com>
2024-10-29 23:18:04 +0800

    Looping : while decrement

---
+++ hari058/Main.java
@@ -0,0 +1,20 @@
+package hari058;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kata : ");
+        String kata = sc.nextLine();
+        String rev = "";
+        
+        int i = kata.length() - 1;
+        while (i >= 0){
+            rev += kata.charAt(i);
+            i--;
+        }
+        System.out.println(rev);
+    }
+    
+}

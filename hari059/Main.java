6bffd1c72805f4111f398ea140b04f845294677f
DaudAja <cpgmng24@gmail.com>
2024-10-30 23:10:45 +0800

    Looping : for each

---
+++ hari059/Main.java
@@ -0,0 +1,12 @@
+package hari059;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {24,23,21};
+        for(int x : array){
+            System.out.print(x+" ");
+        }
+        System.out.println("");
+    }
+    
+}

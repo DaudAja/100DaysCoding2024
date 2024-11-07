7bd10ac003ccc38e7eaae7b478c38cfe53d6bdc6
DaudAja <cpgmng24@gmail.com>
2024-11-07 22:13:41 +0800

    Method Void

---
+++ hari067/Main.java
@@ -0,0 +1,14 @@
+package hari067;
+
+public class Main {
+
+    public void cetakNama() {
+        System.out.println("Informatika");
+    }
+
+    public static void main(String[] args) {
+        Main sc = new Main();
+        sc.cetakNama();
+    }
+
+}

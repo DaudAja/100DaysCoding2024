8ae44afc86737516ee36e63d1a3c48f4eb82de31
DaudAja <cpgmng24@gmail.com>
2024-11-12 23:16:36 +0800

    Method Rekursif

---
+++ hari072/Main.java
@@ -0,0 +1,22 @@
+package hari072;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int hitungFacktorial(int n){
+        if (n <= 1){
+            return 1;
+        } else {
+            return n * hitungFacktorial( n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
+        int factorial = hitungFacktorial(angka);
+        
+        System.out.println("faktorial dari "+angka+" adalah "+factorial);
+    }
+    
+}

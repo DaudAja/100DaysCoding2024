f34ca60d29e7c25ca34000200577457e2bb77563
DaudAja <cpgmng24@gmail.com>
2024-11-15 23:22:26 +0800

    String Method : toLowerCase()

---
+++ hari075/Main.java
@@ -0,0 +1,15 @@
+package hari075;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("kata yang di konversi ke huruf kecil : ");
+        String kata = sc.nextLine().toLowerCase();
+        System.out.println("kata yang Anda Input : "+kata);
+
+    }
+    
+}

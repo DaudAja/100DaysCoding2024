238938e6717b10d8f6fe6f158e785db24623003c
DaudAja <cpgmng24@gmail.com>
2024-11-20 20:41:25 +0800

    String Method : trim()

---
+++ hari80/Main.java
@@ -0,0 +1,11 @@
+package hari80;
+
+public class Main {
+    public static void main(String[] args) {
+        String kata = "    Halo Semua!    ";
+        System.out.println("Kalimat :"+kata);
+        String kataq = kata.trim();
+        System.out.println("Kalimat :"+kataq);
+    }
+    
+}

fac5e165a49974d24170cddf97acb894eca6235d
DaudAja <cpgmng24@gmail.com>
2024-11-26 23:10:07 +0800

    mencetak array satu dimensi

---
+++ hari086/Main.java
@@ -0,0 +1,11 @@
+package hari086;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nilai = {50,60,70,80,100};
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println(nilai[i]);
+        }
+    }
+    
+}

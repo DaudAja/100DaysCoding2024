12034bdde373e3cb62cef37601299f581dff9310
DaudAja <cpgmng24@gmail.com>
2024-12-05 19:14:10 +0800

    Asterisk Pattern : Segitiga Siku dan sebaliknya

--- hari094/Main.java
+++ hari094/Main.java
@@ -1,14 +1,16 @@
 package hari094;
+
 public class Main {
+
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <=  i; k++) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
-            for (int k = i - 1; k >=  1; k--) {
+            for (int k = i - 1; k >= 1; k--) {
                 System.out.print("*");
             }
             System.out.println();
